01818250461e27ccd971d7509c5002358c4a789e
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2021-03-22 10:28:18 +0300

    fixup! KTOR-2178 Remove hidden deprecations from io

--- ktor-io/common/src/io/ktor/utils/io/ByteWriteChannelUtils.kt
+++ ktor-io/common/src/io/ktor/utils/io/ByteWriteChannelUtils.kt
@@ -3,38 +3,70 @@
  */
 package io.ktor.utils.io
 
+import io.ktor.utils.io.bits.*
+
 /**
  * Writes long number and suspends until written.
  * Crashes if channel get closed while writing.
  */
-public expect suspend fun ByteWriteChannel.writeLong(l: Long)
+public suspend fun ByteWriteChannel.writeLong(l: Long) {
+    write(8) { memory, start, _ ->
+        memory.storeLongAt(start, l)
+        8
+    }
+}
 
 /**
  * Writes int number and suspends until written.
  * Crashes if channel get closed while writing.
  */
-public expect suspend fun ByteWriteChannel.writeInt(i: Int)
+public suspend fun ByteWriteChannel.writeInt(i: Int) {
+    write(4) { memory, start, _ ->
+        memory.storeIntAt(start, i)
+        4
+    }
+}
 
 /**
  * Writes short number and suspends until written.
  * Crashes if channel get closed while writing.
  */
-public expect suspend fun ByteWriteChannel.writeShort(s: Short)
+public suspend fun ByteWriteChannel.writeShort(s: Short) {
+    write(2) { memory, start, _ ->
+        memory.storeShortAt(start, s)
+        2
+    }
+}
 
 /**
  * Writes byte and suspends until written.
  * Crashes if channel get closed while writing.
  */
-public expect suspend fun ByteWriteChannel.writeByte(b: Byte)
+public suspend fun ByteWriteChannel.writeByte(b: Byte) {
+    write(1) { memory, start, _ ->
+        memory.storeAt(start, b)
+        1
+    }
+}
 
 /**
  * Writes double number and suspends until written.
  * Crashes if channel get closed while writing.
  */
-public expect suspend fun ByteWriteChannel.writeDouble(d: Double)
+public suspend fun ByteWriteChannel.writeDouble(d: Double) {
+    write(8) { memory, start, _ ->
+        memory.storeDoubleAt(start, d)
+        8
+    }
+}
 
 /**
  * Writes float number and suspends until written.
  * Crashes if channel get closed while writing.
  */
-public expect suspend fun ByteWriteChannel.writeFloat(f: Float)
+public suspend fun ByteWriteChannel.writeFloat(f: Float) {
+    write(4) { memory, start, _ ->
+        memory.storeFloatAt(start, f)
+        4
+    }
+}
