8a47f05624418674b5384fb55c9b3dd36445f9b4
Oleg Yukhnevich <whyoleg@gmail.com>
2024-09-17 22:47:00 +0300

    Commonize ktor-network-tls

--- ktor-network/ktor-network-tls/common/src/io/ktor/network/tls/OID.kt
+++ ktor-network/ktor-network-tls/common/src/io/ktor/network/tls/OID.kt
@@ -1,10 +1,8 @@
 /*
-* Copyright 2014-2021 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
-*/
+ * Copyright 2014-2024 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
 
 package io.ktor.network.tls
-
-import io.ktor.util.*
 
 public data class OID(public val identifier: String) {
     public val asArray: IntArray = identifier.split(".", " ").map { it.trim().toInt() }.toIntArray()
--- ktor-network/ktor-network-tls/jsAndWasmShared/src/io/ktor/network/tls/TLSClientSessionNative.kt
+++ ktor-network/ktor-network-tls/jsAndWasmShared/src/io/ktor/network/tls/TLSClientSessionNative.kt
@@ -1,6 +1,6 @@
 /*
-* Copyright 2014-2021 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
-*/
+ * Copyright 2014-2024 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
 
 package io.ktor.network.tls
 
@@ -8,10 +8,12 @@ import io.ktor.network.sockets.*
 import io.ktor.utils.io.*
 import kotlin.coroutines.*
 
-internal expect suspend fun openTLSSession(
+internal actual suspend fun openTLSSession(
     socket: Socket,
     input: ByteReadChannel,
     output: ByteWriteChannel,
     config: TLSConfig,
     context: CoroutineContext
-): Socket
+): Socket {
+    error("TLS sessions are not supported on Js/WasmJs platform.")
+}
