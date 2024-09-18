a68969ecfce86cb9c3b115070d0f6ef4359130ed
Oleg Yukhnevich <whyoleg@gmail.com>
2024-09-18 18:40:55 +0300

    Support multiple client engines for JS/WasmJs

--- ktor-client/ktor-client-core/jsAndWasmShared/src/io/ktor/client/HttpClientJs.kt
+++ ktor-client/ktor-client-core/jsAndWasmShared/src/io/ktor/client/HttpClientJs.kt
@@ -1,10 +1,10 @@
 /*
- * Copyright 2014-2019 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2014-2024 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
 package io.ktor.client
 
-import io.ktor.client.engine.js.*
+import io.ktor.client.engine.*
 import io.ktor.utils.io.*
 
 /**
@@ -16,4 +16,10 @@ import io.ktor.utils.io.*
 @KtorDsl
 public actual fun HttpClient(
     block: HttpClientConfig<*>.() -> Unit
-): HttpClient = HttpClient(JsClient(), block)
+): HttpClient = HttpClient(FACTORY, block)
+
+@OptIn(InternalAPI::class)
+private val FACTORY = engines.firstOrNull() ?: error(
+    "Failed to find HTTP client engine implementation: consider adding client engine dependency. " +
+        "See https://ktor.io/docs/http-client-engines.html"
+)
