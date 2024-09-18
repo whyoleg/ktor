8f298dfe84784685135ee0b39ca31da546311904
Oleg Yukhnevich <whyoleg@gmail.com>
2024-09-18 18:42:01 +0300

    DEBUG LOGGING FOR NODEJS

--- ktor-network/jsAndWasmShared/src/io/ktor/network/sockets/SocketEngine.jsAndWasmShared.kt
+++ ktor-network/jsAndWasmShared/src/io/ktor/network/sockets/SocketEngine.jsAndWasmShared.kt
@@ -47,6 +47,7 @@ private class SocketEngineImpl(
         val incoming = Channel<JsBuffer>(Channel.UNLIMITED)
         val context = coroutineContext + Job(coroutineContext.job)
         socket.on("connect", fun() {
+            println("NETWORK.CLIENT.SOCKET[${socket.hashCode()}]: connect")
             // TODO unix address
             cont.resume(
                 SocketImpl(
@@ -66,20 +67,25 @@ private class SocketEngineImpl(
             )
         })
         socket.on("error", fun(error: JsError) {
+            println("NETWORK.CLIENT.SOCKET[${socket.hashCode()}]: error $error")
             if (cont.isActive) cont.resumeWithException(IOException("Failed to connect", error.toThrowable()))
             else context.job.cancel("Socket error", error.toThrowable())
         })
         socket.on("timeout", fun() {
+            println("NETWORK.CLIENT.SOCKET[${socket.hashCode()}]: timeout")
             if (cont.isActive) cont.resumeWithException(SocketTimeoutException("timeout"))
             else context.job.cancel("Socket timeout", SocketTimeoutException("timeout"))
         })
         socket.on("end", fun() {
+            println("NETWORK.CLIENT.SOCKET[${socket.hashCode()}]: end")
             incoming.close()
         })
         socket.on("close", fun(_: Boolean) {
+            println("NETWORK.CLIENT.SOCKET[${socket.hashCode()}]: close")
             context.job.cancel("Socket closed")
         })
         socket.on("data", fun(data: JsBuffer) {
+            println("NETWORK.CLIENT.SOCKET[${socket.hashCode()}]: data $data")
             incoming.trySend(data)
         })
     }
@@ -88,22 +94,31 @@ private class SocketEngineImpl(
         val server = createServer(CreateServerOptions {})
         val incomingSockets = Channel<Socket>(Channel.UNLIMITED)
         val serverContext = coroutineContext + SupervisorJob(coroutineContext.job)
+        server.on("listening", fun() {
+            println("NETWORK.SERVER[${server.hashCode()}]: listening")
+        })
         server.on("connection", fun(socket: NodejsSocket) {
+            println("NETWORK.SERVER[${server.hashCode()}]: connection $socket")
             val incoming = Channel<JsBuffer>(Channel.UNLIMITED)
             val context = serverContext + Job(serverContext.job)
             socket.on("error", fun(error: JsError) {
+                println("NETWORK.SERVER.SOCKET[${socket.hashCode()}]: error $error")
                 context.job.cancel("Socket error", error.toThrowable())
             })
             socket.on("timeout", fun() {
+                println("NETWORK.SERVER.SOCKET[${socket.hashCode()}]: timeout")
                 context.job.cancel("Socket timeout", SocketTimeoutException("timeout"))
             })
             socket.on("end", fun() {
+                println("NETWORK.SERVER.SOCKET[${socket.hashCode()}]: end")
                 incoming.close()
             })
             socket.on("close", fun(_: Boolean) {
+                println("NETWORK.SERVER.SOCKET[${socket.hashCode()}]: close")
                 context.job.cancel("Socket closed")
             })
             socket.on("data", fun(data: JsBuffer) {
+                println("NETWORK.SERVER.SOCKET[${socket.hashCode()}]: data")
                 incoming.trySend(data)
             })
 
@@ -126,12 +141,15 @@ private class SocketEngineImpl(
             )
         })
         server.on("close", fun() {
+            println("NETWORK.SERVER[${server.hashCode()}]: close")
             serverContext.job.cancel("Server closed")
         })
         server.on("error", fun(error: JsError) {
+            println("NETWORK.SERVER[${server.hashCode()}]: error $error")
             serverContext.job.cancel("Server failed", error.toThrowable())
         })
         server.on("drop", fun(_: ServerConnectionDrop) {
+            println("NETWORK.SERVER[${server.hashCode()}]: drop")
             // TODO?
         })
         server.listen(ServerListenOptions {
